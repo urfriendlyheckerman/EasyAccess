ef03cf6e5c1b08930d51e6ab712443ee85ce7a96
urfriendlyheckerman <51868298+urfriendlyheckerman@users.noreply.github.com>
2020-05-23 20:27:38 +1200

    Updated main file
    Removed some code to fix it up and finish the project.

--- main.sh
+++ main.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
-#start of script - EasyAccess 
-#This script is used for when Linux users are lazy or new to the system and need to use the cli for their couchdb database, this tool utilizes curl for easy use. This tool may be painful to make but who cares lol
-
-#TODO:
-#Add a feature so it remembers your database ip and/or if you change database ip's then you can enter a new one and it will remember it
-#Use node.js and/or python with this to help, maybe idk
 
 #root check
-#if [ "$(id -u)" != 0 ]; then 
-  #echo "This script requires root. 'sudo su $0'"
-  #exit 1
-#fi
+if [ "$(id -u)" != 0 ]; then 
+  echo "This script requires root. 'sudo su $0'"
+  exit 1
+fi
 
 #VARS
-  #random_vars
-TIME=2.5  
-  #colours
+TIME=2.5
+
 BLUE='\e[34m'
 YELLOW='\e[33m'
 GREEN='\e[32m'
@@ -25,7 +18,7 @@ CYAN='\e[36m'
 GRAY='\e[90m'
 MAGENTA='\e[35m'
 END='\e[0m'
-  #text format
+
 DIM='\e[2m'
 UL='\e[4m'
 HIDE='\e[8m'
@@ -34,10 +27,10 @@ BOLD='\e[1m'
 REV='\e[7m'
 #END_OF_VARS
 
-#if [ ! $1 ]; then echo "Usage: ./main.sh <dbip> <dbport>"
+if [ ! $1 ]; then echo "Usage: ./main.sh <dbip> <dbport>"
 
-#dbip=$1
-#dbport=$2
+dbip=$1
+dbport=$2
 
 #functions
 banner() {
@@ -78,9 +71,6 @@ menu() {
   echo "[2] Create a Database"
   echo "[3] Delete a Database"
   echo "[4] Get Database Info"
-  echo "[5] Create a Database User"
-  echo "[6] Documents"
-  
 }
 
 chdb() {
@@ -98,58 +88,6 @@ dbinf() {
   read -p "Database Name: " dbname3
   curl -X GET http://$dbip:$dbport/$dbname3
 }
-cuser() {
-  read -p "Username you want to create: " user
-  #finding a way with curl xD
-}
-doc() {
-  #whole new menu haha
-  menu2() {
-    clear
-    echo "This is the document parts of the program! In this area you will be able to control all your documents. Have fun ;) "
-    echo "-----------------------------------------------------------------"
-    echo ""
-    echo "NOTE - For this you will need to know the basic syntax of JSON!"
-    sleep 5
-    clear
-    echo ""
-    echo "|------------------|"
-    echo "| Document Options |"
-    echo "|------------------|"
-    echo ""
-    echo "[1] Create Document"
-    echo "[2] Delete Document"
-    echo "[3] Edit Document"
-    echo "[4] Download Document"
-    echo "[0] Go Back"
-  }
-  cdoc() {
-    echo -n "Whats the Database you want to use: "
-    read dbname4
-    echo -n "ID for Document: "
-    read idnum
-    echo -n "Now enter the Document Contents: "
-    read doccon
-    curl -X PUT http://$dbip:$dbport/$dbname4/"$idnum" -d ' { $doccon } '
-  }                                    
-  roptions2() {
-    local choice
-    read -p "Choose an option: " choice2
-    case $choice2 in
-      1) cdoc ;;
-      2) deldoc ;;
-      3) edoc ;;
-      4) ddoc ;;
-      0) gbk ;;
-      *) echo "Error..."
-    esac
-  }
-  while true
-  do
-    menu2
-    roptions2
-  done
-}
 
 roptions() {
   local choice 
@@ -159,8 +97,6 @@ roptions() {
     2) cdb ;;
     3) deldb ;;
     4) dbinf ;;
-    5) cuser ;;
-    6) doc ;;
     99) exit 1 ;; 
     *) echo "Error..."
   esac  
@@ -171,7 +107,7 @@ banner
 trap '' SIGNIT SIGQUIT SIGTSTP
 
 while true
-do
-  menu 
-  roptions
+  do
+    menu
+    roptions
 done
